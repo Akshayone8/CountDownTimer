cacceea415673dbde22c0b222d7ac04c338a71c5
Akshayone8 <62467248+Akshayone8@users.noreply.github.com>
2021-02-19 15:48:32 +0530

    First Release

--- script.js
+++ script.js
@@ -7,63 +7,62 @@ let btn = document.getElementById('btn');
 let reset = document.getElementById('res');
 let paused=document.getElementById('pas');
 let resume=document.getElementById('resume');
+let h=document.getElementById('heading');
 
 let cPause;
 
+
 function validate(){
-if(days.value==0&&hours.value==0&&minutes.value==0&&seconds.value==0){
-  days.value=0;
-  hours.value=0;
-  minutes.value=0;
-  seconds.value=0;
-}else if(seconds.value!=0){
+
+if(seconds.value==0){
+  if(minutes.value>0){
+    minutes.value--;
+    seconds.value=59;
+  }
+  else if(hours.value>0){
+    minutes.value=59;
+    seconds.value=59;
+    hours.value--;
+  }else if(days.value>0){
+    hours.value=24;
+    days.value--;
+  }else{
+   h.innerHTML='Time Over';
+  }
+}else{
   seconds.value--;
-}else if(minutes.value!=0&&seconds.value==0){
-  minutes.value--;
-  seconds.value=59;
-}else if(hours.value!=0&&minutes.value==0){
-  hours.value--;
-  minutes.value=60;
-}else if(days.value!=0&&hours.value==0){
-  days.value--;
-  hours.value=24;
 }
-return;
+
 }
 
 
 btn.addEventListener('click',function(e){
   e.preventDefault()
+  
   cPause=setInterval(function(){validate()},1000);
 })
 
 paused.addEventListener('click',function(){
+ 
   clearInterval(cPause);
+
 })
 
 resume.addEventListener('click',function(){
+ 
   cPause=setInterval(function(){validate()},1000);
 })
 
 reset.addEventListener('click',function(e){
-  // e.preventDefault()
+  e.preventDefault()
   days.value=0;
   hours.value=0;
   minutes.value=0;
   seconds.value=0;
-  // clearInterval(null);
+ 
+  
+  
 })
 
 
 
-
-
-// function validate(e){
-//  e.preventDeat
-// }
-
-// let b=document.getElementById('btn');
-
-// b.addEventListener('click',validate)
-
-// setInterval(validate,1000);
